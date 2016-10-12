5d8381cc7b45aa5c3ad0caad96aecc2c3c0a756f
H. Z. Sababa <hzsababa@outlook.com>
2016-10-12 21:31:00 +0300

    Added list of namespaces I use

---
+++ NameSpacesIUse.cs
@@ -0,0 +1,20 @@
+﻿#region List of namespaces
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms; //Reference required to be added
+using System.Drawing;
+using System.Media;
+using System.Collections.Generic;
+using System.Threading;
+#endregion
+
+namespace NamespacesIUse
+{
+    class NameSpacesIUse
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
