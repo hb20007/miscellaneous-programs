cbf400fec7dfe6dfdc9e60a36b2eaa8d1636de98
H. Z. Sababa <hzsababa@outlook.com>
2016-10-30 12:38:46 +0300

    Added RifleBeautySalon

---
+++ RifleBeautySalon/RifleBeautySalon/Program.cs
@@ -0,0 +1,27 @@
+﻿/// <file>
+/// <author>Hanna Sababa</author>
+/// <datecreated>2016-10-29</datecreated>
+/// <summary>
+/// Contains Main(). Launches an instance of RifleBeautySalonForm
+/// </summary>
+/// </file>
+
+using System;
+using System.Windows.Forms;
+
+namespace RifleBeautySalon
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RifleBeautySalonForm());
+        }
+    }
+}
